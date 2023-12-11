507d97bea556a9bd60d1cbdbde3c53f3fb01faa4
Hammad Bhatti <hammad.bhatti1996@gmail.com>
2023-12-11 19:49:25 +0400

    added fetch, async await, and also getting json data from local file using fetch API with async method

--- fetch.js
+++ fetch.js
@@ -1,30 +1,45 @@
 console.log("fetch rainbow")
-// fetch("picture.jpg")      // fetch data
-//     .then(data => {         // get response and look at the resulting promise
-//         // console.log(data);
-//         return data.blob();
-//     })
-//     .then(blob =>{          //data here
-//         // console.log(blob)
-//         document.getElementById("rainbow").src= URL.createObjectURL(blob);
-//     })
-//     .catch(error =>{
-//         console.error("Error!!!!!!!!!!");
-//         console.error(error)
-//     })
+fetch("picture.jpg")      // fetch data
+    .then(data => {         // get response and look at the resulting promise
+        // console.log(data);
+        return data.blob();
+    })
+    .then(blob =>{          //data here
+        // console.log(blob)
+        document.getElementById("rainbow").src= URL.createObjectURL(blob);
+    })
+    .catch(error =>{
+        console.error("Error!!!!!!!!!!");
+        console.error(error)
+    })
     
 
 
     console.log("ASYnC")
 
     async function catchRainbow(){
-        const response = await fetch("./picture.jpg");
+        const response = await fetch("./download.jpeg");
         const blob = await response.blob();
-        document.getElementById("rainbow").src = URL.createObjectURL(blob)
+        document.getElementById("rainbow2").src = URL.createObjectURL(blob)
     }
 
 
+    async function jsonData (){
+        const response = await fetch("./currencies.json");
+        const data = await response.json();
+        console.log(data["AED"])
+         document.getElementById("data").innerHTML = JSON.stringify(data);
+    }
+
+
+    jsonData ().then(console.log("data")).catch(error=>console.error(error))
+
   catchRainbow()
+    .then(console.log("YAY!"))
+    .catch(error =>{
+        console.error("Error!!!!!!!!!!");
+        console.error(error)
+    })
 
 //    Call fetch
 //  get response
