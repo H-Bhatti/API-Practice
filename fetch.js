9c9a3106a3bf216e77eef087a7be641aba84ae78
Hammad Bhatti <hammad.bhatti1996@gmail.com>
2023-12-11 19:29:26 +0400

    did same with async awit

--- fetch.js
+++ fetch.js
@@ -1,20 +1,30 @@
 console.log("fetch rainbow")
-fetch("picture.jpg")      // fetch data
-    .then(data => {         // get response and look at the resulting promise
-        console.log(data);
-        return data.blob();
-    })
-    .then(blob =>{          //data here
-        console.log(blob)
-        document.getElementById("rainkbow").src= URL.createObjectURL(blob);
-    })
-    .catch(error =>{
-        console.error("Error!!!!!!!!!!");
-        console.error(error)
-    })
+// fetch("picture.jpg")      // fetch data
+//     .then(data => {         // get response and look at the resulting promise
+//         // console.log(data);
+//         return data.blob();
+//     })
+//     .then(blob =>{          //data here
+//         // console.log(blob)
+//         document.getElementById("rainbow").src= URL.createObjectURL(blob);
+//     })
+//     .catch(error =>{
+//         console.error("Error!!!!!!!!!!");
+//         console.error(error)
+//     })
     
 
 
+    console.log("ASYnC")
+
+    async function catchRainbow(){
+        const response = await fetch("./picture.jpg");
+        const blob = await response.blob();
+        document.getElementById("rainbow").src = URL.createObjectURL(blob)
+    }
+
+
+  catchRainbow()
 
 //    Call fetch
 //  get response
